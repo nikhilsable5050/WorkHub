07b87d0180631c10a2d53bdd1d3851ad641a66c3
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-09-22 18:52:10 +0530

    Add dept repo

---
+++ src/main/java/nikhilsable/WorkHub/repository/DepartmentRepository.java
@@ -0,0 +1,13 @@
+package nikhilsable.WorkHub.repository;
+
+import nikhilsable.WorkHub.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Optional<Department> findByNameIgnoreCase(String name);
+
+    boolean existsByNameIgnoreCase(String name);
+}
