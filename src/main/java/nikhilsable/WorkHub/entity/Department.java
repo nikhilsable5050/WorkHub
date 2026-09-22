e0cf17cc10d0d336729514fbcba125c7e74367eb
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-09-22 18:41:55 +0530

    Add department entity

---
+++ src/main/java/nikhilsable/WorkHub/entity/Department.java
@@ -0,0 +1,31 @@
+package nikhilsable.WorkHub.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+        name = "departments",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_department_name",
+                        columnNames = "name"
+                )
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long departmentId;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+}
