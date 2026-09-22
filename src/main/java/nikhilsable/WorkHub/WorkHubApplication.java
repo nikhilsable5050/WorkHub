3e1406d83cbd6c2b027e492312a6769be6be8054
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-09-22 18:26:56 +0530

    Initial project setup

---
+++ src/main/java/nikhilsable/WorkHub/WorkHubApplication.java
@@ -0,0 +1,13 @@
+package nikhilsable.WorkHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nikhilsable/WorkHub/WorkHubApplicationTests.java
@@ -0,0 +1,13 @@
+package nikhilsable.WorkHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
