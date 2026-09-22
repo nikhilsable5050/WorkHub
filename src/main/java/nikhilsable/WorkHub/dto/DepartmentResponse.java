e79aab8f704ea43ef8c5a9a3c615e75ce43e0619
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-09-22 18:54:22 +0530

    Add dept response

---
+++ src/main/java/nikhilsable/WorkHub/dto/DepartmentResponse.java
@@ -0,0 +1,17 @@
+package nikhilsable.WorkHub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentResponse {
+
+    private Long departmentId;
+
+    private String name;
+}
